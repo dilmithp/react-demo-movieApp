41e8eaae451be368c244b40fc599570626ed7e4e
dilmithp <dilmithp@outlook.com>
2025-06-19 16:45:17 +0530

    addImgs

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ const App = () => {
         <main>
             <div className="pattern">
                 <div className="wrapper">
+                    <header>
                     <h1>Find Ur <span className="text-gradient">Favourite</span> Movies Here</h1>
+                    </header>
                 </div>
             </div>
         </main>
