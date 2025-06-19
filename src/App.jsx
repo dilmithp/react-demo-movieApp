c1cd7114868efc9e10ae6cb4f864b2b97b554f35
dilmithp <dilmithp@outlook.com>
2025-06-19 15:32:09 +0530

    InitialSetupTailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <main>
+            <div className="pattern">
+                <div className="wrapper">
+                    <h1>Find Ur <span className="text-gradient">Favourite</span> Movies Here</h1>
+                </div>
+            </div>
+        </main>
+    )
+}
+export default App
